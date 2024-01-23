c495436ce2c7ef68031f4bb21ed9421ad055e50f
ViktorKula <vicyru@yandex.ru>
2024-01-23 18:33:39 +0300

    from work

--- NewsPaper/mc_donalds/models.py
+++ NewsPaper/mc_donalds/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from mc_donalds.resources import POSITIONS, cashier
+from datetime import datetime
+
 
 class Order(models.Model):
     time_in = models.DateTimeField(auto_now_add=True)
@@ -10,30 +13,43 @@ class Order(models.Model):
 
     products = models.ManyToManyField("Product", through='ProductOrder')
 
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:  # если завершён, возвращаем разность объектов
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:  # если ещё нет, то сколько длится выполнение
+            return (datetime.now(timezone.utc) - self.time_in).total_seconds() // 60
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.0)
-
-director = 'DI'
-admin = 'AD'
-cook = 'CO'
-cashier = 'CA'
-cleaner = 'CL'
-
-POSITIONS = [
-    (director, 'Директор'),
-    (admin, 'Администратор'),
-    (cook, 'Повар'),
-    (cashier, 'Кассир'),
-    (cleaner, 'Уборщик')
-]
 
 class Staff(models.Model):
     full_name = models.CharField(max_length=255)
     position = models.CharField(max_length=2, choices = POSITIONS, default = cashier)
     labor_contract = models.IntegerField(default = 0)
 
+    def get_last_name(self):
+        return self.full_name.split()[0]
+
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+    _amount = models.IntegerField(default=1, db_column='amount')
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
