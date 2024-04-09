35d72fcb77bb92d809214c7ca829dca521a92c0c
ViktorKula <vicyru@yandex.ru>
2024-04-09 22:12:04 +0300

    locale

--- NewsPaper/NewsPaper01/settings.py
+++ NewsPaper/NewsPaper01/settings.py
@@ -131,6 +131,9 @@ CACHES = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'django.middleware.locale.LocaleMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,6 +146,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'NewsPaper01.urls'
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 TEMPLATES = [
     {
